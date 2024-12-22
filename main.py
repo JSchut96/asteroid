7bc38c422f8cc6f4534c158198785da757646104
JSchut96 <jeroen.schut@hotmail.com>
2024-12-22 23:33:37 +0100

    added asteroidfield generation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,14 +18,19 @@ def main():
     # init pygame groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     # setup clock
     clock = pygame.time.Clock()
     dt = 0
 
+    # init elements
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
