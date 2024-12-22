5403177a021c21bdac1bff52dcd4939144070604
JSchut96 <jeroen.schut@hotmail.com>
2024-12-22 23:01:11 +0100

    Added circleshape and player class. Draw player to screen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,12 +17,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
         screen.fill((0,0,0,0))
+
+        player.draw(screen)
+
+
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
